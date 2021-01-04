8fb0a9103b301b6deb026fd0b76fce716fd874b7
ZachyDev <mosetizachary001@gmail.com>
2021-01-04 18:35:12 +0300

    added code confirmation block

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const signUser = () => {
-    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+const signUser = () => {    
+    const recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
     const phoneNumber = document.getElementsByClassName('phone')[0].value;
+    let code = document.getElementsByClassName('code')[0].value;
 
-    firebase.auth().signInWithPhoneNumber(phoneNumber,window.recaptchaVerifier)
-        .then(res => {
-            window.alert('code sent')
+    firebase.auth().signInWithPhoneNumber(phoneNumber,recaptchaVerifier)
+        .then(confirmationResult => {
+            window.alert('code sent');
+            window.confirmationResult = confirmationResult;
+
+            confirmationResult.confirm(code)
+            .then(() => {
+                alert('User signed in successfully');
+            })
+            .catch(err => alert(err.message))
         })
         .catch(err => window.alert(err.message))
+
+
+
+     
 }
+
